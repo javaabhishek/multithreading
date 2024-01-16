22950f498441f2d7b403f33d20a514f972f2e832
javaabhishek <java.abhishek.patil@gmail.com>
2024-01-16 10:07:26 +0530

    Multi-threading

---
+++ src/main/java/com/soft/multithreading/InventoryHistory.java
@@ -0,0 +1,22 @@
+package com.soft.multithreading;
+
+public class InventoryHistory{
+        Integer sysId;
+        Integer qtyAmount;
+
+        public Integer getSysId() {
+            return sysId;
+        }
+
+        public void setSysId(Integer sysId) {
+            this.sysId = sysId;
+        }
+
+        public Integer getQtyAmount() {
+            return qtyAmount;
+        }
+
+        public void setQtyAmount(Integer qtyAmount) {
+            this.qtyAmount = qtyAmount;
+        }
+    }
